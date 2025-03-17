05d53231e5e4e6da2384de31481a2f2ab9f31f8b
jhums04 <jbonganciso04@gmail.com>
2025-03-17 10:04:51 +0800

    remove unsused components

--- 1.react-essentials/src/App.jsx
+++ 1.react-essentials/src/App.jsx
@@ -1,5 +1,4 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Core", "Crucial"];
