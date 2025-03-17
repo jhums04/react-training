d04bc81940e00393d678efd751d63b38092fc01a
jhums04 <jbonganciso04@gmail.com>
2025-03-17 16:38:37 +0800

    Rendering content dynamically with added data

--- 1.react-essentials/src/App.jsx
+++ 1.react-essentials/src/App.jsx
@@ -3,9 +3,10 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState(null);
   function handleSelect(selectedBtn) {
     setSelectedTopic(selectedBtn);
     console.log(selectedBtn);
@@ -39,7 +40,17 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}> Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {!selectedTopic ? (
+            <p>Please select a topic</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
