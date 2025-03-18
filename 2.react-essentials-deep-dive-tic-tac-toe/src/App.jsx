7223f4b91ca50fb288c2e7c51ed9c370622dc394
jhums04 <jbonganciso04@gmail.com>
2025-03-18 14:25:50 +0800

    added player component

--- 2.react-essentials-deep-dive-tic-tac-toe/src/App.jsx
+++ 2.react-essentials-deep-dive-tic-tac-toe/src/App.jsx
@@ -1,8 +1,15 @@
+import Player from "./components/Player.jsx";
+
 function App() {
   return (
-    <div>
-      <p>coming soon</p>
-    </div>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player playerName="Player 1" symbol="X" />
+          <Player playerName="Player 2" symbol="O" />
+        </ol>
+      </div>
+    </main>
   );
 }
 
